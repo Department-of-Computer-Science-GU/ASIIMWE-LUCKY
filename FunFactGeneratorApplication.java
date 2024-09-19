c52da85041365e6728e2fe5f637e33eef66efcc0
0788178984 <159613584+0788178984@users.noreply.github.com>
2024-09-19 15:21:58 +0300

    Add files via upload

---
+++ FunFactGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.example.funfactgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunFactGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunFactGeneratorApplication.class, args);
+    }
+}
